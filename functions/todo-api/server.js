0316224c0cb3e44a1ec8c4e0ed23c0381abb0682
dynamo-whiz <dynamowhiz@gmail.com>
2020-12-31 18:27:10 +0100

    Added mongoose & cleaned up
    1. Added mongoose to begin creating a test API in preparation for the UI.
    2. Removed unnecessary server-local.js file and removed ununsed dependency.

--- functions/todo-api/server.js
+++ functions/todo-api/server.js
@@ -4,8 +4,17 @@ const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const router = express.Router()
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', () => console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('Established connection with MongoDB cloud!')
+})
 
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' })
--- server-local.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const app = require('./functions/todo-api/server')
-const port = 4000
-app.listen(port, () => {
-  console.log(`Started Netlify-ExpressJS app on Port ${port}!`)
-})
