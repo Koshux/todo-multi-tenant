5d377db291f5084ca673acbe6297caf2a9687af7
dynamo-whiz <dynamowhiz@gmail.com>
2021-01-03 16:33:44 +0100

    Added React Routing
    Implemented basic routing for React to show pages based on the URL.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" exact component={ Home } />
+          <Route path="/login" exact component={ Login } />
+          <Route path="/register" exact component={ Register } />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
