7bb449b1df4d6cf0e476acf8921c5b6db9841363
Koshux <lanzonprojects@gmail.com>
2021-01-05 02:27:33 +0100

    Almost finished login page
    1. Almost completed login page shell.
    2. Requires hooking up textfields and button to state and request.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
 import './App.css'
-import {
-  Link,
-  Route,
-  BrowserRouter as Router,
-  Switch
-} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import Register from './pages/Register'
 import ProtectedRoute from './components/protected-route'
+import React from 'react'
+import Register from './pages/Register'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+const theme = createMuiTheme({
+  overrides: {
+    MuiTypography: {
+      h1: {
+        'font-family': 'BitterPro'
+      }
+    }
+  }
+})
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <ProtectedRoute
-            component={Home}
-            exact
-            path="/home"
-          />
-          <Route
-            component={ Login }
-            exact
-            path="/"
-          />
-          <Route
-            component={ Register }
-            exact
-            path="/register"
-          />
-          <Route
-            path="*"
-            component={() => "404 - Page Not Found"}
-          />
-        </Switch>
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Container minWidth="sm" className="background">
+          {/* <div className="App"> */}
+            <Switch>
+              <ProtectedRoute
+                component={Home}
+                exact
+                path="/home"
+              />
+              <Route
+                component={ Login }
+                exact
+                path="/"
+              />
+              <Route
+                component={ Register }
+                exact
+                path="/register"
+              />
+              <Route
+                path="*"
+                component={() => "404 - Page Not Found"}
+              />
+            </Switch>
+          {/* </div> */}
+        </Container>
+      </ThemeProvider>
     </Router>
-  );
+  )
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,106 @@
 import React from 'react'
 import auth from '../components/auth'
+import Typography from '@material-ui/core/Typography'
+// import Card from '../components/Card'
+
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import LockIcon from '@material-ui/icons/Lock'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    marginTop: '15vh',
+    marginLeft: '40vh',
+    marginRight: '40vh',
+    height: '30vh',
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}))
 
 export default function LoginPage (props) {
+  const classes = useStyles()
+
   return (
-    <div>
-      Login Page
-      <button
-        onClick={() => {
-          auth.login(() => {
-            props.history.push('/home')
-          })
-        }}
-      >
-        Login
-      </button>
-    </div>
+    <React.Fragment>
+      <Typography variant="h1">
+        TODO
+      </Typography>
+
+      <Typography variant="subtitle2">
+        Your personal productivity zone.
+      </Typography>
+
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography variant="h4">
+                Sign In
+              </Typography>
+              <TextField
+                id="standard-full-width"
+                label="Username"
+                placeholder="Enter your username"
+                fullWidth
+                margin="normal"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircleIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                autoComplete="current-password"
+                fullWidth
+                id="standard-full-width"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                label="Password"
+                margin="normal"
+                placeholder="Enter your password"
+                type="password"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="start">
+                      <LockIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Button
+                color="secondary"
+                fullWidth
+                onClick={() => {
+                  auth.login(() => {
+                    props.history.push('/home')
+                  })
+                }}
+                variant="contained"
+                endIcon={<ArrowForwardIosIcon />}
+              >
+                Sign in
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
   )
 }
